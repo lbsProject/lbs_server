54c57d4df529bcc985f03411b043a12a4cbd97d5
kanginkug <rkddlsrnr1234@gmail.com>
2023-10-27 12:17:01 +0900

    test

--- src/main/java/lbs/lbs/LbsApplication.java
+++ src/main/java/lbs/lbs/LbsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LbsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LbsApplication.class, args);
+		System.out.println("sd");
 	}
 
 }
