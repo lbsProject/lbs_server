a0604f2c61b620cfb06c9703272a08f381067831
SonJimin <lijnar100@naver.com>
2023-11-02 21:35:56 +0900

    Chore : 브랜치 테스트 용으로 BoardController 의 Sout 수정23.11.02.21:35
    마스터 브랜치에서 LbsApplication.java sout 테스트 마스터로 수정

--- src/main/java/lbs/lbs/LbsApplication.java
+++ src/main/java/lbs/lbs/LbsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LbsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("테스트1");
+		System.out.println("테스트2");
 		SpringApplication.run(LbsApplication.class, args);
 	}
 }
