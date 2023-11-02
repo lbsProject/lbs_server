bc3938157a1d1fa9253bab27e31834d17cb320db
kanginkug <rkddlsrnr1234@gmail.com>
2023-11-02 22:40:33 +0900

    Feat: mysql - application.properties 샘플 작성 & crud 테스트 controller, service, repository, entity 생성

---
+++ src/main/java/lbs/lbs/domain/User.java
@@ -0,0 +1,17 @@
+package lbs.lbs.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class User {
+    @Id @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    // 게터, 세터 및 다른 필수 메서드
+
+}
