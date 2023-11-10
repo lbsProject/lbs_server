5cbaf11dbcf999729c8e590af1f87b3eb795b877
kanginkug <rkddlsrnr1234@gmail.com>
2023-11-11 06:51:47 +0900

    feat: user entity 생성 & joinType enum, authority enum 생성
    joinType enum 에는 카카오, 네이버, 구글이 있고
    authority enum 에는 관리자, 일반 유저가 있다.

--- src/main/java/lbs/lbs/entity/User.java
+++ src/main/java/lbs/lbs/entity/User.java
@@ -3,15 +3,28 @@ package lbs.lbs.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 @Entity
 @Getter @Setter
 public class User {
-    @Id @Column(name = "user_id")
+    @Id @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Nullable
+    private String userId;
+    @Nullable
     private String username;
     private String email;
+    @Nullable
+    private String password;
+    private int birth;
+    private String phone;
+    private Authority auth;
+    private int pwErrChk;
+    private boolean userYN;
+    private JoinType joinType;
     // 게터, 세터 및 다른 필수 메서드
 
 }
