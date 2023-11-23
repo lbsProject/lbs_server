3ed5ec3fb122ac2fa1bbd6ce4a8aed86878e8713
kanginkug <rkddlsrnr1234@gmail.com>
2023-11-23 16:01:24 +0900

    feat: OAuth2 로그인 시 받는 유저정보 인터페이스 생성

---
+++ src/main/java/lbs/lbs/config/oauth/provider/OAuth2UserInfo.java
@@ -0,0 +1,15 @@
+package lbs.lbs.config.oauth.provider;
+
+// OAuth2.0 제공자들 마다 응답해주는 속성값이 달라서 공통으로 만들어준다.
+public interface OAuth2UserInfo {
+    String getProviderId();
+    String getProvider();
+    String getEmail();
+    String getName();
+    String getPhone();
+    String getNickName();
+    String getUserId();
+    int getBirth();
+    String getPassword();
+}
+
