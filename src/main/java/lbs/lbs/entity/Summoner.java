05d274aaf923742a5808fca1f5e3bf40e28d0e8f
kanginkug <rkddlsrnr1234@gmail.com>
2023-11-28 19:32:36 +0900

    fix: 생성자를 만드는 어노테이션 추가

--- src/main/java/lbs/lbs/entity/Summoner.java
+++ src/main/java/lbs/lbs/entity/Summoner.java
@@ -3,10 +3,14 @@ package lbs.lbs.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Summoner {
     @Id @Column(name = "summoner_id")
     private String id;
@@ -15,4 +19,6 @@ public class Summoner {
     private String puuid;
     private String name;
     private int summonerLevel;
+
+
 }
