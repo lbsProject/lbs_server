6223506fef6c9bd9b80ac104f3ad4a1c5a6a357b
kanginkug <rkddlsrnr1234@gmail.com>
2023-11-28 19:34:50 +0900

    feat: SummonerResponseDto 생성
    api 호출시 전체 데이터를 받아오는 dto 생성
    엔티티에는 필요한 데이터만 넣기위해 dto를 생성했습니다.

---
+++ src/main/java/lbs/lbs/dto/SummonerResponseDto.java
@@ -0,0 +1,15 @@
+package lbs.lbs.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SummonerResponseDto {
+    private String id;
+
+    private String accountId;
+    private String revisionDate;
+    private String profileIconId;
+    private String puuid;
+    private String name;
+    private int summonerLevel;
+}
