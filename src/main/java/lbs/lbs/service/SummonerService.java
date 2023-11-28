6371a362c38af4ffb23159e460753931920db0cd
kanginkug <rkddlsrnr1234@gmail.com>
2023-11-28 19:37:03 +0900

    fix: 상태코드가 200이 아니면 아이디가 존재하지 않는다는 에러메세지 출력 & summonerResponseDto를 이용해 api 호출 데이터를 받아오고 이미 db에 있다면 db 데이터를 리턴하고 없다면 불러온 데이터를 저장하고 리턴하는 코드로 수정

--- src/main/java/lbs/lbs/service/SummonerService.java
+++ src/main/java/lbs/lbs/service/SummonerService.java
@@ -1,7 +1,10 @@
 package lbs.lbs.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lbs.lbs.dto.SummonerResponseDto;
 import lbs.lbs.entity.Summoner;
+import lbs.lbs.repository.SummonerRepository;
+import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -14,9 +17,11 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 
 @Service
+@RequiredArgsConstructor
 public class SummonerService {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    private final SummonerRepository summonerRepository;
     private final CloseableHttpClient httpClient = HttpClients.createDefault();
 
     @Value("${riot.api.key}")
@@ -25,19 +30,24 @@ public class SummonerService {
     public Summoner callRiotAPISummonerByName(String summonerName) {
         try {
             HttpGet request = new HttpGet("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ summonerName + "?api_key=" + myKey);
-            System.out.println("myKey : " + myKey);
             try (CloseableHttpResponse response = httpClient.execute(request)) {
                 int statusCode = response.getStatusLine().getStatusCode();
-                System.out.println("HTTP Status Code: " + statusCode);
 
                 if (statusCode != 200) {
-                    return null;
+                    throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
                 }
-
                 HttpEntity entity = response.getEntity();
-                Summoner summoner = objectMapper.readValue(entity.getContent(), Summoner.class);
-                System.out.println("Summoner Data: " + summoner);
+                SummonerResponseDto summonerResponseDto = objectMapper.readValue(entity.getContent(), SummonerResponseDto.class);
 
+                if(summonerRepository.existsById(summonerResponseDto.getId())){
+                    return summonerRepository.findById(summonerResponseDto.getId()).orElseThrow(() ->
+                            new NullPointerException("해당 아이디가 존재하지 않습니다2."));
+
+                }
+                Summoner summoner = new Summoner(summonerResponseDto.getId(), summonerResponseDto.getProfileIconId(),
+                        summonerResponseDto.getPuuid(),summonerResponseDto.getName(), summonerResponseDto.getSummonerLevel());
+
+                summonerRepository.save(summoner);
                 return summoner;
             }
         } catch (IOException e) {
